54902b6d96e52b67e8c3dcfe5f8eb08c8758621f
Adam Patterson <awpatterson217@gmail.com>
2017-09-12 02:26:43 -0500

    CSS for register and login tabs

--- public/resources/js/activeLinks.js
+++ public/resources/js/activeLinks.js
@@ -1,16 +1,17 @@
 const currentPage = window.location.pathname.slice(1);    
 
 let removeAllActive = function(){
-    document.getElementById("properties").classList.remove("active");
-    document.getElementById("app").classList.remove("active");
-    document.getElementById("contact").classList.remove("active");
+	document.getElementById("properties").classList.remove("active");
+	document.getElementById("app").classList.remove("active");
+	document.getElementById("contact").classList.remove("active");
 }
 
 let addActive = function(){
-   
-    if (typeof(currentPage) !== 'undefined' && currentPage !== null && currentPage !== ''){
-        document.getElementById(`${currentPage}`).classList.add("active");
-    }
+	if(typeof(currentPage) !== 'undefined' && currentPage !== null && currentPage !== ''){
+		if(currentPage !== 'home')
+			document.getElementById(`${currentPage}`).classList.add("active");
+	}
 }
+
 removeAllActive();
 addActive();
