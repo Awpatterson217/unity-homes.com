dae398d6d4accd2e65f05e33db5037524729064f
Adam Patterson <awpatterson217@gmail.com>
2017-09-12 00:03:34 -0500

    Adding login, register pages, CSS for responsive nav

---
+++ public/resources/js/activeLinks.js
@@ -0,0 +1,15 @@
+const currentPage = window.location.pathname.slice(1);    
+
+let removeAllActive = function(){
+    document.getElementById("properties").classList.remove("active");
+    document.getElementById("app").classList.remove("active");
+    document.getElementById("contact").classList.remove("active");
+}
+
+let addActive = function(){
+    if(currentPage !== 'home')        
+        document.getElementById(`${currentPage}`).classList.add("active");    
+}
+
+removeAllActive();
+addActive();
--- public/routes/index.js
+++ public/routes/index.js
@@ -7,10 +7,16 @@ module.exports = function(app) {
  app.get('/properties', function(req, res) {
    res.render('properties');
  });
- app.get('/pay', function(req, res) {
-    res.render('pay');
+ app.get('/app', function(req, res) {
+    res.render('app');
   });
   app.get('/contact', function(req, res) {
     res.render('contact');
+  });
+  app.get('/register', function(req, res) {
+    res.render('register');
+  });
+  app.get('/login', function(req, res) {
+    res.render('login');
   });
 };
