93475efe63bf71cdbf7fffa0f3f9e1b7762f4918
Adam Patterson <awpatterson217@gmail.com>
2017-09-17 17:55:29 -0500

    Adding password restrictions

--- routes/login.js
+++ routes/login.js
@@ -1,76 +1,88 @@
 "use strict";
 const express              = require('express');
-const { check, validationResult, sanitize }            = require('express-validator/check');
+const { check }            = require('express-validator/check');
+const { validationResult } = require('express-validator/check');
 const { matchedData }      = require('express-validator/filter');
+const validator            = require('validator');
 
 const router  = express.Router();
-const passExp = /[0-9]{1}[A-Z]{1}[a-z]{1}\S/
+const passExp = /^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z0-9]{8,20}$/
 
 router.get('/login', function(req, res) {
   res.render('login');
 });
   
-router.post('/login', [
-    // Check validity
-    check("email", "Invalid email")
-    .isEmail()
-    .exists(),
-    check("password", "invalid password")
-        .exists()
-        .isLength({ min: 8 })
-        .custom((value,{req, loc, path}) => {
-            if (value !== req.body.password) {
-                // throw error if passwords do not match
-                throw new Error("Passwords don't match");
-            } else 
-                return value;
-        })
-],
-function(req, res, next) {
+router.post('/login', function(req, res, next) {
 
-  const errors = validationResult(req);
+  // Is user input a string?
+  if(typeof req.body.email !== 'string')
+    return res.render('login', { invalid: true });
+  if(typeof req.body.password !== 'string')
+    return res.render('login', { invalid: true });
 
-  if (!errors.isEmpty()) {
-    // LOG ERRORS HERE
-    res.render('login', { invalid: true });
+  // remove white space
+  let trimEmail = validator.trim(req.body.email);
+  let trimPw    = validator.trim(req.body.password);
+
+  // Is user input empty? 
+  if(validator.isEmpty(trimEmail))
+      return res.render('login', { invalid: true });
+  if(validator.isEmpty(trimPw))
+      return res.render('login', { invalid: true });
+
+  // Replace <, >, &, ', " and / with HTML entities.
+  let escapedEmail = validator.escape(trimEmail);
+  let escapedPw    = validator.escape(trimPw);
+
+  let authorized = false;
+  let owner      = false;
+  let tenant     = false;
+  let time;
+
+  // Is password 8-20 characters
+  if(!(validator.isLength(escapedPw, {min: 8, max: 20}))){
+    console.log('Not long enough');
+    return res.render('login', { invalid: true });
   }
 
-  if (errors.isEmpty()) {
-    let authorized = false;
-    let owner      = false;
-    let tenant     = false;
-    let time;
-    const NOW      = new Date().getTime();
-    //console.log(NOW);   // LOG credentials and time here
-    // BELOW IS JUST A TEST
-    if(req.body.email && req.body.password){
-      if(req.body.email === "tenant@unity-homes.com"){
-        if(req.body.password === "password"){
-          authorized = true;
-          tenant     = true;
-          time       = NOW;
-        }
-      }
-      if(req.body.email === "owner@unity-homes.com"){
-        if(req.body.password === "password"){
-          authorized = true;
-          owner      = true;
-          time       = NOW;
-        }
-      }
-    }
-    if(authorized){
-      if(owner){
-        // Create session here
-        return res.render('owner');
-      } 
-      if(tenant){
-        // Create session here
-        return res.render('tenant');
-      }
-    }
-    res.render('login', { invalid: true });
+  // Does password have:
+  // 1 uppercase
+  // 1 lowercase
+  // 1 number
+  if(!(validator.matches(escapedPw, passExp))){
+    return res.render('login', { invalid: true });
   }
+
+  // Log credentials and time here
+  const NOW      = new Date().getTime();
+  //console.log(NOW);
+
+  // BELOW IS JUST A TEST
+  if(escapedEmail === "tenant@unity-homes.com"){
+    if(escapedPw=== "Password1"){
+      authorized = true;
+      tenant     = true;
+      time       = NOW;
+    }
+  }
+  if(escapedEmail === "owner@unity-homes.com"){
+    if(escapedPw=== "Password1"){
+      authorized = true;
+      owner      = true;
+      time       = NOW;
+    }
+  }
+  if(authorized){
+    if(owner){
+      return res.render('owner');
+    } 
+    if(tenant){
+      return res.render('tenant');
+    }
+  }
+  res.render('login', { invalid: true });
+
+
 },
 function(req, res) {
     res.redirect('/home');
