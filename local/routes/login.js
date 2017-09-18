031e2767b0a174d606753ebdf7bb621ef7eb1cde
Adam Patterson <awpatterson217@gmail.com>
2017-09-18 03:27:32 -0500

    Error handling for csurf

--- local/routes/login.js
+++ local/routes/login.js
@@ -61,7 +61,7 @@ router.post('/login', csrfProtection, function(req, res, next) {
 });
 
 // handle csrf errors specifically
-app.use(function(err, req, res, next) {
+router.use(function(err, req, res, next) {
     if (err.code !== 'EBADCSRFTOKEN') return next(err);
     res.status(403).json({"error": "session has expired or tampered with"});
 });
