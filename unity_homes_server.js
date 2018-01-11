eed9d62addf5d1992ddacce21cb208a2035ae0a3
Adam Patterson <awpatterson217@gmail.com>
2018-01-10 23:55:42 -0600

    Adding default 404

--- unity_homes_server.js
+++ unity_homes_server.js
@@ -105,6 +105,17 @@ for (let apiKey in APIs) {
   app.use('/api', APIs[apiKey]);
 }
 
+// TODO Custom error handling and logs
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+
+// assume 404 since no middleware responded
+app.use(function(req, res, next){
+  res.status(404).render('404', { url: req.originalUrl });
+});
+
 const server = app.listen(PORT, HOST, () => {
   const host = server.address().address;
   const port = server.address().port;
