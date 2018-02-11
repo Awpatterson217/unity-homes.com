126a36fc8efc05401134ede424f3ce28e75d98c7
Adam Patterson <awpatterson217@gmail.com>
2018-02-11 17:13:57 -0600

    Documenting server

--- dev_server.js
+++ dev_server.js
@@ -9,24 +9,68 @@ const helmet     = require('helmet');
 const session    = require('express-session');
 const ejs        = require('ejs');
 
+// I require all routes and API endpoints from
+// the index.js located in /local/routes and /local/api
 const routes = require('./local/routes');
 const APIs   = require('./local/api');
-
+// Custom authorization check as middleware,
+// I tell express to use this middleware before
+// passing any reuests to their designated API route.
 const { checkAuth } = require('./local/node_modules/lib/middleware');
-
+// Creating the parsing middleware that will
+// be used as middleware later in the application.
 const urlEncParser = bodyParser.urlencoded({
   extended: false
 });
 const jsonParser = bodyParser.json();
-
+// The length of time before a session expires
 const sessionTime = 1000000;
+// These values are hidden in production,
+// but they are the host and port numbers used
+// to initializethe server at the bottom of this page.
 const port        = 3000;
 const host        = '127.0.0.4';
+// In production, this shouldn't
+// be publicly visible.
+// This is the secret used to sign the session ID cookie.
+// DOCS: https://github.com/expressjs/session#secret
 const SECRET      = 'pretendSecret';
 
+// Create an Express app by
+// calling express().
 const app = express();
+//
+//    *Note: You can also create mini express apps
+//           by calling the express.router() method, and then passing
+//           the router into the main express application as middleware
+//           with the app.use() methods. I did this for each route for both the
+//           the API, and routes to the views at /local/routes and /local/api.
+//
+//           Alternatively, you can actually just create another express application
+//           and pass that into our main Express app as well.
+//
 
-// Cookie sessions for development
+// Ejs templates
+// All files to be served must
+// use the .ejs extension.
+app.set('view engine', 'ejs');
+app.set('views', [
+  path.join(__dirname, 'dist/views'),
+  path.join(__dirname, 'dist/dashboard'),
+]);
+
+// app.use(my-middleware), means use this middleware for
+// all routes that follow. 
+
+// You could also do app.use(my-route, my-middleware),
+// in which case the same pricipal applies but for the
+// given route only.
+
+// Express basically just places all middleware
+// into an array in the order given, and then calls
+// the next() method.
+
+// Cookie sessions for development.
 app.use(session({
   secret: SECRET,
   resave: true,
@@ -37,54 +81,87 @@ app.use(session({
     maxAge  : sessionTime,
   }
 }));
+// For production, I am using a NoSQL DB (Redis).
+// Server side sessions are more secure than cookies.
+// The only reason I would store sessions using
+// cookies in production would be for cross domain 
+// compatibility.
+//
+//    *note: I do (or will) use cookies for CSRF protection.
+//           Basically, everytime the user requests a page
+//           which contains a form, I send a randomly generated
+//           string and store it as a hidden input field.
+//           I will not accept the forms submission if the randomly
+//           string was not what I expect. This helps protect
+//           from cross-site scripting attacks. 
+//
 
-// Security
+
+// helmet is a middleware that
+// handles most of your common
+// security concerns.
+// **This must be used first, because
+// the order of your route matters.
 app.use(helmet());
+// Helmet restricts the `X-Powered-By header`,
+// for you automatically, but if you only wanted
+// to use a subset of helmet you can. 
+//
+// I explictly called the .hidePoweredBy() method,
+// because it makes me feel safe, even though
+// it isn't necessary. :)
 app.use(helmet.hidePoweredBy());
 
-// Parsing
+// For greater security, I am also using NGINX
+// as a reverse-proxy, and then passing
+// acceptable requests to the express server.
+
+// Include parsing middleware
+// for JSON, and URL encoded data
+// only.
 app.use(urlEncParser); 
 app.use(jsonParser); 
-
-// Static Files
-app.use('/css', express.static(__dirname + '/dist/css'));
-app.use('/js', express.static(__dirname + '/dist/js'));
+// Serve static files from /css, /js, /images, /assets
+// Must use express.static for this to work.
+app.use('/css'   , express.static(__dirname + '/dist/css'));
+app.use('/js'    , express.static(__dirname + '/dist/js'));
 app.use('/images', express.static(__dirname + '/dist/media/images'));
 app.use('/assets', express.static(__dirname + '/dist/dashboard/assets'));
 
-// Check for authorization for all api calls
+// Check for authorization for all
+// API calls before routed to API.
+// Order matters.
 app.use('/api', checkAuth);
 
-// Adding API
+// Adding API after authorization check
+// (imported from /local)
 for (let apiKey in APIs) {
   app.use('/api', APIs[apiKey]);
 }
 
 // Adding front-end routes
+// (imported from /local)
 for (let routeKeys in routes) {
   app.use(routes[routeKeys]);
 }
 
-// Ejs templating
-app.set('view engine', 'ejs');
-app.set('views', [
-  path.join(__dirname, 'dist/views'),
-  path.join(__dirname, 'dist/dashboard'),
-]);
-
 // Default error handler
+// This works, because Express will
+// recognize that there are four parameters.
 app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(404).render('error', { url: req.originalUrl });
 });
 
 // Assume 404 since no middleware responded
+// This works, because it is the last route added
+// to the Express application. Order matters.
 app.use(function(req, res, next){
   res.status(404).render('error', { url: req.originalUrl });
 });
-
+// Start Express server
 const server = app.listen(port, host, () => {
-  // const host = server.address().address;
-  // const port = server.address().port;
-  // console.log(`Server running at http://${host}:${port}`);
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`Proxy running at http://${host}:${port}`);
 });
--- unity_homes_server.js
+++ unity_homes_server.js
@@ -10,54 +10,72 @@ const redis      = require("redis");
 const session    = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const ejs        = require('ejs');
-
+// Custom authorization check as middleware,
+// I tell express to use this middleware before
+// passing any reuests to their designated API route.
+const { checkAuth } = require('./local/node_modules/lib/middleware');
+// I require all routes and API endpoints from
+// the index.js located in /local/routes and /local/api
 const routes = require('./local/routes');
 const APIs   = require('./local/api');
-
-// Authorization check for API
-const { checkAuth } = require('./local/node_modules/lib/middleware');
-
-// Parsers
+// Creating the parsing middleware that will
+// be used as middleware later in the application.
 const urlEncParser = bodyParser.urlencoded({
   extended: false
 });
 const jsonParser   = bodyParser.json();
+// Length of time to maintain a session.
+const ttl = 180;  
+// I keep the production Port and Local IP
+// secret for security.
+// They are the host and port numbers used
+// to initializethe server at the bottom of this page.
+const PORT = process.env.UNITY_PORT;
+let HOST   = process.env.UNITY_HOST;
+// This is the secret used to sign the session ID cookie.
+// DOCS: https://github.com/expressjs/session#secret
+let SECRET = process.env.UNITY_SECRET;
 
+if (typeof SECRET !== 'undefined'){
+  SECRET = SECRET.trim();
+} else {
+  console.log("SECRET is undefined");
+}
+if (typeof HOST !== 'undefined') {
+  HOST = HOST.trim();
+} else {
+  console.log("HOST is undefined");
+}
+
+// Create a Redis client which will get
+// passed to the express-session middleware.
 const client       = redis.createClient();
 const redisOptions = {
   client,
   ttl
 }
-// Seconds
-const ttl     = 180;  
-// TODO: Read from external file
-const sslPort = 3443;
-let PORT   = process.env.UNITY_PORT;
-let HOST   = process.env.UNITY_HOST;
-let SECRET = process.env.UNITY_SECRET;
 
 client.on("error", function(err) {
   // TODO: LOG
   console.log("Error " + err);
 });
 
-if (typeof SECRET !== 'undefined'){
-  SECRET = SECRET.trim();
-} else {
-  // TODO: LOG
-  console.log("SECRET is undefined");
-}
+// Create an Express app by calling express().
+const app = express();
+//
+//    *Note: You can also create mini express apps
+//           by calling the express.router() method, and then passing
+//           the router into the main express application as middleware
+//           with the app.use() methods. I did this for each route for both the
+//           the API, and routes to the views at /local/routes and /local/api.
+//
+//           Alternatively, you can actually just create another express application
+//           and pass that into our main Express app as well.
+//
 
-if (typeof HOST !== 'undefined') {
-  HOST = HOST.trim();
-} else {
-  // TODO: LOG
-  console.log("HOST is undefined");
-}
-
-const app     = express();
+// Pass the Express application and Redis client
+// to the express-limiter module.
 const limiter = require('express-limiter')(app, client);
-
 // Limit requests to 150 per hour per IP
 limiter({
   lookup: ['connection.remoteAddress'],
@@ -65,14 +83,27 @@ limiter({
   expire: 1000 * 60 * 60
 })
 
-// Ejs templating
+// Ejs templates
+// All files to be served must
+// use the .ejs extension.
 app.set('view engine', 'ejs');
 app.set('views', [
   path.join(__dirname, 'dist/views'),
   path.join(__dirname, 'dist/dashboard'),
 ]);
 
-// Server side sessions with Redis
+// app.use(my-middleware), means use this middleware for
+// all routes that follow. 
+
+// You could also do app.use(my-route, my-middleware),
+// in which case the same pricipal applies but for the
+// given route only.
+
+// Express basically just places all middleware
+// into an array in the order given, and then calls
+// the next() method.
+
+// Server side sessions with Redis.
 app.use(
   session({
     store : new RedisStore(redisOptions),
@@ -81,55 +112,77 @@ app.use(
     resave: false
   })
 );
+// Sessions can be accessed when handling requests like:
+// 
+//   const my-middleware = function(req, res, next) {
+//     const my-variable = req.session.my-variable; 
+//   }
+//
+//   An example of this can be seen at /local/routes/login/index.js  
+//
+//   *note: You can write your own middleware, just made sure
+//          you pass in `next` as a parameter. for example, I have written
+//          custom middleware that is located at /local/node_modules/lib/middleware.js
 
-// Security
+// helmet is a middleware that
+// handles most of your common
+// security concerns.
+// **This must be used first, because
+// the order of your route matters.
 app.use(helmet());
+// Helmet restricts the `X-Powered-By header`,
+// for you automatically, but if you only wanted
+// to use a subset of helmet you can. 
+//
+// I explictly called the .hidePoweredBy() method,
+// because it makes me feel safe, even though
+// it isn't necessary. :)
 app.use(helmet.hidePoweredBy());
 
-// Parsing
+// For greater security, I am also using NGINX
+// as a reverse-proxy, and then passing
+// acceptable requests to the express server.
+
+// Include parsing middleware
+// for JSON, and URL encoded data
+// only.
 app.use(urlEncParser); 
 app.use(jsonParser); 
-
-// Static Files
-app.use('/css', express.static(__dirname + '/dist/css'));
-app.use('/js', express.static(__dirname + '/dist/js'));
+// Serve static files from /css, /js, /images, /assets
+// Must use express.static for this to work.
+app.use('/css'   , express.static(__dirname + '/dist/css'));
+app.use('/js'    , express.static(__dirname + '/dist/js'));
 app.use('/images', express.static(__dirname + '/dist/media/images'));
 app.use('/assets', express.static(__dirname + '/dist/dashboard/assets'));
-
-// Check for authorization for all api calls
+// Check for authorization for all
+// API calls before routed to API.
+// Order matters.
 app.use('/api', checkAuth);
-
-// Adding API
+// Adding API after authorization check
+// (imported from /local)
 for (let apiKey in APIs) {
   app.use('/api', APIs[apiKey]);
 }
-
 // Adding front-end routes
+// (imported from /local)
 for (let routeKeys in routes) {
   app.use(routes[routeKeys]);
 }
-
-// Ejs templating
-app.set('view engine', 'ejs');
-app.set('views', [
-  path.join(__dirname, 'dist/views'),
-  path.join(__dirname, 'dist/dashboard'),
-]);
-
 // Default error handler
+// This works, because Express will
+// recognize that there are four parameters.
 app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(404).render('error', { url: req.originalUrl });
 });
-
 // Assume 404 since no middleware responded
+// This works, because it is the last route added
+// to the Express application. Order matters.
 app.use(function(req, res, next){
   res.status(404).render('error', { url: req.originalUrl });
 });
-
-// Server
+// Start Express server
 app.listen(port, host);
-
 
 //        TODO
 
